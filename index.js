cb57d277649c1950f0730578f8816334882bb364
Jered Parkin <jeredparkin@gmail.com>
2021-10-14 22:11:51 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function introduction(name = "Josh") {
-    return `Hi, my name is ${name}.`
+function introduction(name){
+    return `Hi, my name is ${name}.`;
 }
 
-function introductionWithLanguage (name = "Josh",language = "Ember.js") {
-    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+function introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
 
-function introductionWithLanguageOptional (name,language = "JavaScript") {
-    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
